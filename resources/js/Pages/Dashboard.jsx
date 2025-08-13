bcb222bed09a5727888688526aed7ce462c0f73c
BlackTrojan-kali <chendjousil@gmail.com>
2025-08-14 00:24:07 +0100

    not working download corrected

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -158,15 +158,7 @@ const Dashboard = ({ entreprises }) => {
                   </TableCell>
 
                   <TableCell className="py-3 text-gray-500 text-theme-sm gap-2 flex dark:text-gray-400">
-                    {/* Bouton de téléchargement */}
-                    <Link
-                      href={`/companies/${entreprise.id}/download`} // Exemple de route pour le téléchargement
-                      className="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
-                    >
-                      <FontAwesomeIcon icon={faDownload} />
-                    </Link>
-
-                    {/* Bouton d'édition */}
+                  
                     <button
                       onClick={() => handleOpenEditModal(entreprise)} // Ouvre la modale d'édition avec l'entreprise sélectionnée
                       className="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
